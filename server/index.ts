a5d1743ba6d88f40b66b77fa12dab02338c92b80
adrian-ri <107512062+adrian-ri@users.noreply.github.com>
2025-04-14 20:08:08 -0600

    challenge: Create basic endpoints
    This will allow adding data to the table and getting data as well.
    Tested endpoints using Postman.
    https://purpleinnovationllc.postman.co/workspace/My-Workspace~5f3a4c46-7752-4ef8-882b-932fda24ffc9/collection/23841261-93397643-5949-4f4f-af28-8512799b73d9?action=share&creator=23841261

--- server/index.ts
+++ server/index.ts
@@ -17,9 +17,85 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+// GET /organization - get all organizations
+app.get("/organizations", (req, res) => {
   const rows = db.prepare("SELECT * FROM organizations").all();
   res.json({ message: "Welcome to the server! 🎉", rows });
+});
+
+// GET /account- get all accounts
+app.get("/accounts", (req, res) => {
+  const rows = db.prepare("SELECT * FROM accounts").all();
+  res.json({ message: "Welcome to the server! 🎉", rows });
+});
+
+// GET /deal - get all deals 
+app.get("/deals", (req, res) => {
+  const rows = db.prepare("SELECT * FROM deals").all();
+  res.json({ message: "Welcome to the server! 🎉", rows });
+});
+
+// POST /organization - Create a new organization
+app.post('/organizations',(req, res) => {
+  const { name } = req.body;
+
+  try {
+    const stmt = db.prepare(`
+      INSERT INTO organizations (name)
+      VALUES (?)
+    `);
+    const result = stmt.run(name);
+
+    res.json({ 
+      message: "Organization Created",
+      organizationId: result.lastInsertRowid 
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create Organization" });
+  }
+});
+
+// POST /account - Create a new account
+app.post('/accounts',(req, res) => {
+  const { name } = req.body;
+
+  try {
+    const stmt = db.prepare(`
+      INSERT INTO accounts (name)
+      VALUES (?)
+    `);
+    const result = stmt.run(name);
+
+    res.json({ 
+      message: "Account Created",
+      accountId: result.lastInsertRowid 
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create Account" });
+  }
+});
+
+// POST /deal - Create a new deal
+app.post('/deals',(req, res) => {
+  const { organization_id, account_id } = req.body;
+
+  try {
+    const stmt = db.prepare(`
+      INSERT INTO deals (organization_id, account_id)
+      VALUES (?, ?)
+    `);
+    const result = stmt.run(organization_id, account_id);
+
+    res.json({ 
+      message: "Deal Created",
+      dealId: result.lastInsertRowid 
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to create deal" });
+  }
 });
 
 app.listen(port, () => {
